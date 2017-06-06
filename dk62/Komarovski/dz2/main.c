d562ef190e679a712f76d6cb713a146225df1e18
NickolaiKom <nikolaykom98@gmail.com>
2017-06-06 20:17:22 +0300

    Add files via upload

--- dk62/Komarovski/dz2/main.c
+++ dk62/Komarovski/dz2/main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tool.h"
 
+int main()
+{
+	printf("Max amount of piramides: 5\n");
+	int amount = anAmount();
+	
+	List *Head = createList(amount);
+	
+	if(Head != NULL)
+	{
+		printf("\nPiramide has 4 dots\n\n");
+		
+		for(int i = 0; i < amount; i++)
+		{
+			if(addPiramide(Head) != 0)
+			{
+				deleteList(Head);
+				return 1;
+			}
+		}
+		
+		printList(Head);
+		deleteList(Head);
+		
+		return 0;	
+	}
+	return 1;
+}
